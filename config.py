52cf0915f5bfab7d15303175f45dfce5caee1eac
Long Zhang (Kanch) <kanchisme@gmail.com>
2019-01-09 21:33:34 +0800

    redesign add ruleset popup #!

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 from np_tools.loadconfidentials import loadConfidential
 from np_tools.configmanager import loadKey
 
+# web UI preference
+CONSOLE_PWD = loadKey("console_password")
+FIRST_SETUP = eval(loadKey("FIRST_SETUP"))
+
 # others
 DEBUG_MODE = eval(loadKey("DEBUG_MODE"))
 
@@ -9,16 +13,13 @@ DEBUG_MODE = eval(loadKey("DEBUG_MODE"))
 SENDGRID_MAIL_API_KEY = ""
 if DEBUG_MODE:
     SENDGRID_MAIL_API_KEY = loadConfidential("confidentials/SendGridAPIKey.txt")
+    FIRST_SETUP = False
 else:
     SENDGRID_MAIL_API_KEY = loadKey("sendgrid_mail_api_key")
 
 # email preference
 EMAIL_SENDING_TIME = loadKey("EMAIL_SENDING_TIME")     # in UTC+0
 EMAIL_RECIVIER = loadKey("EMAIL_RECIVIER")
-
-# web UI preference
-CONSOLE_PWD = loadKey("console_password")
-FIRST_SETUP = eval(loadKey("FIRST_SETUP"))
 
 # news cache
 MAX_CACHED_NEWS = eval(loadKey("MAX_CACHED_NEWS"))
