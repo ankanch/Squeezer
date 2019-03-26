ff9dfb073b73fe0808d5d634569a0f8006913b46
Long Zhang (Kanch) <kanchisme@gmail.com>
2019-03-26 23:12:01 +0800

    add system status template

--- app.py
+++ app.py
@@ -139,6 +139,6 @@ def api_updatetime():
 
 if __name__ == '__main__':
     if CFG.DEBUG_MODE:
-        app.run(host="127.0.0.1",port=1030)
+        app.run(host="127.0.0.1",port=1000,debug=True)
     else:
         app.run(host="0.0.0.0", port=1030)
